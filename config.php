c99fba055ee45ec847335fdac4e312287cefceb6
toando249 <toando249@gmail.com>
2020-08-31 15:36:50 +0700

    Add files via upload

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$DBHOST = getenv('ec2-54-146-91-153.compute-1.amazonaws.com'); // Set your database host here
-$DBUSER = getenv('alxxzzshxwqpxx'); // Set your database user here
-$DBNAME = getenv('d9nlggmagjuct4'); // Set your database name here
-$DBPW =  getenv('71d719b7c6d572dcbacb57220394b5241d3254c05e2fa11bb97bd24e9aeac60b'); // Set your database password here
+$DBHOST = getenv('qn0cquuabmqczee2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'); // Set your database host here
+$DBUSER = getenv('l6nrrzmm0lbrxnps'); // Set your database user here
+$DBNAME = getenv('pppj7nrypebol76z'); // Set your database name here
+$DBPW =  getenv('amnemzzudp1ylnxo'); // Set your database password here
 $BOT_ID = getenv('5f49cae901c345658fcb4963');
 $TOKEN = getenv('U1ZdYozpo1V8Mry3nmUss1uP6aTFDU6Z2gc3EcVCcwSW7BXwZfngq9HXfle7XaDb');
 
