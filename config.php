c00bdf32b852af0f609fc80675abfc29bc554bb7
toando249 <toando249@gmail.com>
2020-08-31 21:24:14 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $DBHOST = getenv('db4free.net'); // Set your database host here
-$DBUSER = getenv('toando249'); // Set your database user here
-$DBNAME = getenv('toando249'); // Set your database name here
+$DBUSER = getenv('toando2499'); // Set your database user here
+$DBNAME = getenv('toando2499'); // Set your database name here
 $DBPW =  getenv('toan12345678'); // Set your database password here
 $BOT_ID = getenv('5f49cae901c345658fcb4963');
 $TOKEN = getenv('U1ZdYozpo1V8Mry3nmUss1uP6aTFDU6Z2gc3EcVCcwSW7BXwZfngq9HXfle7XaDb');
