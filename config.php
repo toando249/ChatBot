82733c8895b15ddcab747eaa77302ce15086b7aa
toando249 <toando249@gmail.com>
2020-08-31 21:22:04 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$DBHOST = getenv('qn0cquuabmqczee2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306'); // Set your database host here
-$DBUSER = getenv('l6nrrzmm0lbrxnps'); // Set your database user here
-$DBNAME = getenv('pppj7nrypebol76z'); // Set your database name here
-$DBPW =  getenv('amnemzzudp1ylnxo'); // Set your database password here
+$DBHOST = getenv('db4free.net'); // Set your database host here
+$DBUSER = getenv('toando249'); // Set your database user here
+$DBNAME = getenv('toando249'); // Set your database name here
+$DBPW =  getenv('toan12345678'); // Set your database password here
 $BOT_ID = getenv('5f49cae901c345658fcb4963');
 $TOKEN = getenv('U1ZdYozpo1V8Mry3nmUss1uP6aTFDU6Z2gc3EcVCcwSW7BXwZfngq9HXfle7XaDb');
 
